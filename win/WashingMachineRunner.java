e4e5a3af113d793bf6437b2ce48f366deac2cab6
meghamantur1007 <megham.xworkz@gmail.com>
2024-08-07 19:25:07 +0530

    Delete win/WashingMachineRunner.java

--- win/WashingMachineRunner.java
+++
@@ -1,8 +0,0 @@
-class WashingMachineRunner{
-	public static void main(String[] args)
-	{
-		System.out.println("running main in WashingMachineRunner");
-		WashingMachine.rinse();
-		WashingMachine.makenoise();
-	}
-}
